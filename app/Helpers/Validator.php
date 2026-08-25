127c5e39acc91e6721c49fbb6737f5f513760fbd
Tardis74 <svelegzanin3@gmail.com>
2026-08-25 16:42:37 +0300

    Update Validator.php

--- app/Helpers/Validator.php
+++ app/Helpers/Validator.php
@@ -5,16 +5,22 @@ namespace App\Helpers;
 class Validator
 {
     /**
-     * Очищает и проверяет СНИЛС на валидность (11 цифр).
-     * Возвращает очищенную строку из 11 цифр.
-     * @throws \InvalidArgumentException
+     * Очищает и проверяет СНИЛС.
+     * @param string $snils Исходный СНИЛС (может содержать дефисы и пробелы)
+     * @return string Очищенный СНИЛС (11 цифр)
+     * @throws \InvalidArgumentException Если СНИЛС невалидный
      */
     public static function validateSnils(string $snils): string
     {
-        $snils = preg_replace('/[^0-9]/', '', $snils);
-        if (strlen($snils) !== 11) {
-            throw new \InvalidArgumentException('СНИЛС должен содержать ровно 11 цифр.');
+        // Удаляем все нецифровые символы
+        $cleaned = preg_replace('/\D/', '', $snils);
+        
+        if (strlen($cleaned) !== 11) {
+            throw new \InvalidArgumentException('СНИЛС должен содержать 11 цифр.');
         }
-        return $snils;
+        
+        // Здесь можно добавить проверку контрольной суммы СНИЛС, если требуется
+        
+        return $cleaned;
     }
 }
